190e6c89371860b2e131b24fbc1ce5ed844c1a0d
ysh <1299331829@qq.com>
2025-09-03 21:11:56 +0800

    feat: 完成客户端逻辑

---
+++ client.cpp
@@ -0,0 +1,61 @@
+#include <unistd.h>
+#include <cstring>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <time.h>
+
+int main(int argc, char* argv[]) {
+  if (argc != 3) {
+    printf("usage:./client ip port\n");
+    printf("example:./client 192.168.150.128 5085\n\n");
+    return -1;
+  }
+
+  int sockfd;
+  struct sockaddr_in servaddr;
+  char buf[1024];
+
+  if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    printf("socket() failed.\n");
+    return -1;
+  }
+
+  memset(&servaddr, 0, sizeof(servaddr));
+  servaddr.sin_family = AF_INET;
+  servaddr.sin_port = htons(atoi(argv[2]));
+  servaddr.sin_addr.s_addr = inet_addr(argv[1]);
+
+  if (connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) != 0) {
+    printf("connect(%s:%s) failed.\n", argv[1], argv[2]);
+    close(sockfd);
+    return -1;
+  }
+
+  printf("connect ok.\n");
+  printf("开始时间：%d\n", time(0));
+
+  for (int i = 0; i < 10000; ++i) {
+    memset(buf, 0, sizeof(buf));
+    sprintf(buf, "这是第%d个超级女生。", i);
+
+    char tmpbuf[1024];
+    memset(tmpbuf, 0, sizeof(tmpbuf));
+    int len = strlen(buf);
+
+    memcpy(tmpbuf, &len, 4);
+    memcpy(tmpbuf + 4, buf, len);
+
+    send(sockfd, tmpbuf, len + 4, 0);
+
+    recv(sockfd, &len, 4, 0);
+
+    memset(buf, 0, sizeof(buf));
+    recv(sockfd, buf, len, 0);
+
+    printf("recv:%s\n", buf);
+  }
+  printf("结束时间：%d\n", time(0));
+}
--- echoserver.cpp
+++ echoserver.cpp
@@ -2,10 +2,31 @@
 #include <signal.h>
 #include "EchoServer.h"
 
+EchoServer* echoserver;
+
+void Stop(int sig) {
+  printf("sig=%d\n", sig);
+
+  echoserver->Stop();
+  printf("echoserver已停止。\n");
+  delete echoserver;
+  printf("delete echoserver。\n");
+  exit(0);
+}
+
+
 int main(int argc, char* argv[]) {
   if (argc != 3) {
     printf("usage: ./echoserver ip port\n");
     printf("example: ./echoserver 192.168.150.128 5085\n\n");
     return -1;
   }
+
+  signal(SIGTERM, Stop);
+  signal(SIGINT, Stop);
+
+  echoserver = new EchoServer(argv[1], atoi(argv[2]), 3, 2);
+  echoserver->Start();
+
+  return 0;
 }
